7158d42f93ab68bc918e75ffbdec7463b4b8a5b5
eugeneshine1 <eugeneshine1@gmail.com>
2022-06-27 21:52:54 -0400

    final copy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 /**
- * Created by iyasuwatts on 10/17/17.
+ * * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int sum = 0;
+
+        for (int a = 0; a <= n; a++) {
+            sum += a;
+        }
+        System.out.println(sum);
     }
 }
